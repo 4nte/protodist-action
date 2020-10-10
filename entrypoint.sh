4f1225d0dfa8086386df0080d106734b5b0d9cfb
Ante Gulin <8070595+4nte@users.noreply.github.com>
2020-10-10 21:49:10 +0200

    wip

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+if [ -z "$INPUTS_GIT_USER" ]
+then
+    export PROTODIST_GIT_USER=$INPUTS_GIT_USER
+fi
+
+if [ -z "$INPUTS_GIT_BRANCH" ]
+then
+    export PROTODIST_GIT_BRANCH=$INPUTS_GIT_BRANCH
+fi
+
+if [ -z "$INPUTS_PROTO_IMPORT_PATH" ]
+then
+    export PROTODIST_PROTO_IMPORT_PATH=$INPUTS_PROTO_IMPORT_PATH
+fi
+
+if [ -z "$INPUTS_GIT_ACCESS_TOKEN" ]
+then
+    export PROTODIST_GIT_ACCESS_TOKEN=$INPUTS_GIT_ACCESS_TOKEN
+fi
+
+if [ -z "$INPUTS_GIT_TAG" ]
+then
+    export PROTODIST_GIT_TAG=$INPUTS_GIT_TAG
+fi
