f4fc953b63aba156ba37c446c569204efdc7361d
Ante Gulin <8070595+4nte@users.noreply.github.com>
2020-10-10 22:05:49 +0200

    Fix env setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
-if [ -z "$INPUTS_GIT_USER" ]
+if [ -n "$INPUTS_GIT_USER" ]
 then
     export PROTODIST_GIT_USER=$INPUTS_GIT_USER
 fi
 
-if [ -z "$INPUTS_GIT_BRANCH" ]
+if [ -n "$INPUTS_GIT_BRANCH" ]
 then
     export PROTODIST_GIT_BRANCH=$INPUTS_GIT_BRANCH
 fi
 
-if [ -z "$INPUTS_PROTO_IMPORT_PATH" ]
+if [ -n "$INPUTS_PROTO_IMPORT_PATH" ]
 then
     export PROTODIST_PROTO_IMPORT_PATH=$INPUTS_PROTO_IMPORT_PATH
 fi
 
-if [ -z "$INPUTS_GIT_ACCESS_TOKEN" ]
+if [ -n "$INPUTS_GIT_ACCESS_TOKEN" ]
 then
     export PROTODIST_GIT_ACCESS_TOKEN=$INPUTS_GIT_ACCESS_TOKEN
 fi
 
-if [ -z "$INPUTS_GIT_TAG" ]
+if [ -n "$INPUTS_GIT_TAG" ]
 then
     export PROTODIST_GIT_TAG=$INPUTS_GIT_TAG
 fi
