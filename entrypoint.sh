31ab94aa4d41032456ee388a5d143e3c3c5b0ce0
Ante Gulin <8070595+4nte@users.noreply.github.com>
2020-10-10 22:13:11 +0200

    Fix env names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
-if [ -n "$INPUTS_GIT_USER" ]
+if [ -n "$INPUT_GIT_USER" ]
 then
     export PROTODIST_GIT_USER=$INPUTS_GIT_USER
 fi
 
-if [ -n "$INPUTS_GIT_BRANCH" ]
+if [ -n "$INPUT_GIT_BRANCH" ]
 then
     export PROTODIST_GIT_BRANCH=$INPUTS_GIT_BRANCH
 fi
 
-if [ -n "$INPUTS_PROTO_IMPORT_PATH" ]
+if [ -n "$INPUT_PROTO_IMPORT_PATH" ]
 then
     export PROTODIST_PROTO_IMPORT_PATH=$INPUTS_PROTO_IMPORT_PATH
 fi
 
-if [ -n "$INPUTS_GIT_ACCESS_TOKEN" ]
+if [ -n "$INPUT_GIT_ACCESS_TOKEN" ]
 then
     export PROTODIST_GIT_ACCESS_TOKEN=$INPUTS_GIT_ACCESS_TOKEN
 fi
 
-if [ -n "$INPUTS_GIT_TAG" ]
+if [ -n "$INPUT_GIT_TAG" ]
 then
     export PROTODIST_GIT_TAG=$INPUTS_GIT_TAG
 fi
